37f6d3787c4048fb2401c4efa4b9f736482edb92
Lachezar Mihaylov <lachezar.mihaylov@techhuddle.com>
2019-12-02 14:26:18 +0200

    Complete the Image class

--- src/Image.php
+++ src/Image.php
@@ -4,6 +4,10 @@ namespace GraphicalEditor;
 
 use GraphicalEditor\Exceptions\InvalidPixelException;
 
+/**
+ * Class Image
+ * @package GraphicalEditor
+ */
 class Image
 {
     /**
@@ -38,6 +42,12 @@ class Image
         $this->config = array_merge($this->defaultConfig, $config);
     }
 
+    /**
+     * Creates a new canvas.
+     *
+     * @param int $cols
+     * @param int $rows
+     */
     public function createCanvas(int $cols, int $rows): void
     {
         $this->cols = $cols;
@@ -45,33 +55,48 @@ class Image
         $this->resetCanvas();
     }
 
+    /**
+     * Cleans the canvas.
+     */
     public function clearCanvas(): void
     {
         $this->resetCanvas();
     }
 
+    /**
+     * Resets the canvas.
+     */
     protected function resetCanvas(): void
     {
         $this->canvas = array_fill(1, $this->rows, array_fill(1, $this->cols, $this->config['whiteColour']));
     }
 
     /**
-     * @return array
+     * Draws a pixel.
+     *
+     * @param int $x
+     * @param int $y
+     * @param string $colour
+     * @throws InvalidPixelException
      */
-    public function getCanvas(): array
-    {
-        return $this->canvas;
-    }
-
     public function drawPixel(int $x, int $y, string $colour): void
     {
-        if (!isset($this->canvas[$x][$y])) {
-            throw new InvalidPixelException('');
+        if (!isset($this->canvas[$y][$x])) {
+            throw new InvalidPixelException("Invalid pixel at ($x;$y}) coordinates");
         }
 
         $this->canvas[$y][$x] = $colour;
     }
 
+    /**
+     * Draws a vertical line.
+     *
+     * @param int $x
+     * @param int $y1
+     * @param int $y2
+     * @param string $colour
+     * @throws InvalidPixelException
+     */
     public function drawVerticalLine(int $x, int $y1, int $y2, string $colour): void
     {
         $startY = min($y1, $y2);
@@ -82,6 +107,15 @@ class Image
         }
     }
 
+    /**
+     * Draws a horizontal line.
+     *
+     * @param int $x1
+     * @param int $x2
+     * @param int $y
+     * @param string $colour
+     * @throws InvalidPixelException
+     */
     public function drawHorizontalLine(int $x1, int $x2, int $y, string $colour): void
     {
         $startX = min($x1, $x2);
@@ -92,11 +126,78 @@ class Image
         }
     }
 
+    /**
+     * Fills a canvas region.
+     * It implements the flood fill algorithm.
+     *
+     * @param int $x
+     * @param int $y
+     * @param string $colour
+     * @throws InvalidPixelException
+     */
     public function fillRegion(int $x, int $y, string $colour): void
     {
+        $pixels = [];
+        $pixels[] = ['x' => $x, 'y' => $y];
 
+        while (!empty($pixels)) {
+            $next = array_pop($pixels);
+            $x = $next['x'];
+            $y = $next['y'];
+
+            $this->drawPixel($x, $y, $colour);
+            $pixels = array_merge($pixels, $this->getNeighborEmptyPixels($x, $y));
+        }
     }
 
+    /**
+     * Returns a list that contains empty neighbor pixels.
+     *
+     * @param int $x
+     * @param int $y
+     * @return array
+     */
+    protected function getNeighborEmptyPixels(int $x, int $y): array
+    {
+        $pixels = [];
+
+        // check the top pixel
+        if ($this->isEmptyPixel($x, $y - 1)) {
+            $pixels[] = ['x' => $x, 'y' => $y - 1];
+        }
+        // check the bottom pixel
+        if ($this->isEmptyPixel($x, $y + 1)) {
+            $pixels[] = ['x' => $x, 'y' => $y + 1];
+        }
+        // check the left pixel
+        if ($this->isEmptyPixel($x - 1, $y)) {
+            $pixels[] = ['x' => $x - 1, 'y' => $y];
+        }
+        // check the right pixel
+        if ($this->isEmptyPixel($x + 1, $y)) {
+            $pixels[] = ['x' => $x + 1, 'y' => $y];
+        }
+
+        return $pixels;
+    }
+
+    /**
+     * Determines whether a specific pixel is empty (white colour).
+     *
+     * @param int $x
+     * @param int $y
+     * @return bool
+     */
+    public function isEmptyPixel(int $x, int $y): bool
+    {
+        return isset($this->canvas[$y][$x]) && $this->canvas[$y][$x] === $this->config['whiteColour'];
+    }
+
+    /**
+     * Returns string representation.
+     *
+     * @return string
+     */
     public function __toString(): string
     {
         $image = '';
