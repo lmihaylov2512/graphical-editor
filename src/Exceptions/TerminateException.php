e58edb8c29f971ce53c5cbb066a86df1c015ffaa
Lachezar Mihaylov <lachezar.mihaylov@techhuddle.com>
2019-12-02 15:59:11 +0200

    Create a new TerminateException

---
+++ src/Exceptions/TerminateException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GraphicalEditor\Exceptions;
+
+use Exception;
+
+/**
+ * Class TerminateException
+ * @package GraphicalEditor\Exceptions
+ */
+class TerminateException extends Exception
+{
+}
